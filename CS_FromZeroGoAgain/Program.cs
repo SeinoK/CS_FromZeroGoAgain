2e0a1cd79faf21295d863b98c823ba869852696c
Seino <whoroad@126.com>
2017-06-21 16:12:32 +0800

    have change something

--- CS_FromZeroGoAgain/Program.cs
+++ CS_FromZeroGoAgain/Program.cs
@@ -6,46 +6,5 @@ namespace CS_PrintNineTimesNine
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //nine();
-            generateSpace();
-
-            Console.ReadKey();
-        }
-
-        static int Nine()
-        {
-            int mul = 0;
-            string str = "";
-            for (int i = 1; i < 10; i++)
-            {
-                for (int j = i; j < 10; j++)
-                {
-                    mul = i * j;
-                    str = mul.ToString();
-                    //Console.Write(str.Length);
-                    Console.Write(i + "*" + j + "=" + ((str.Length == 1) ? " " : "") + (i * j) + "  ");
-                }
-                Console.WriteLine();
-            }
-            //Console.Write("*");
-            return mul;
-        }
-
-        static void generateSpace()
-        {
-            int NUM = Nine();
-
-            /*if (nine().ToString() != null)
-            {
-                Console.WriteLine("This is a Test");
-            }
-            */
-
-            Console.WriteLine(NUM);
-        }
-        
-
     }
 }
---
+++ CS_PrintNineTimesNine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS_PrintNineTimesNine
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Nine();
+            Console.ReadKey();
+        }
+        static int Nine()
+        {
+            int mul = 0;
+            string str = "";
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = i; j < 10; j++)
+                {
+                    mul = i * j;
+                    str = mul.ToString();
+                    Console.Write(i + "*" + j + "=" + ((str.Length == 1) ? " " : "") + (i * j) + "  ");
+                }
+                Console.WriteLine();
+            }
+            return mul;
+        }
+
+    }
+}
