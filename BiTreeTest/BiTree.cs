170edb213c2e53cc2c45a6dd4b7498f4a66b437c
Seino <Seino@A0>
2017-11-30 21:22:47 +0800

    Add a B-Tree structure

---
+++ BiTreeTest/BiTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BiTreeTest
+{
+    class BiTree<T>
+    {
+        private T[] data;
+        private int count;
+        public BiTree(int capcity)
+        {
+            data = new T[capcity];
+        }
+        public bool Add(T item)
+        {
+            if (count >= data.Length)
+                return false;
+            data[count] = item;
+            count++;
+            return true;
+        }
+    }
+}
---
+++ BiTreeTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BiTreeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] data = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J' };
+            BiTree<char> tree = new BiTree<char>(10);
+            for (int i = 0; i < data.Length; i++)
+            {
+                tree.Add(data[i]);
+            }
+        }
+    }
+}
